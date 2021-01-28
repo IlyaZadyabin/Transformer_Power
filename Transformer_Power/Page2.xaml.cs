7fc721a7e8ce16c04836c81ac8c022c23f7f071e
IlyaZadyabin <z.ilya.007@gmail.com>
2021-01-28 16:39:09 +0300

    graph is beautifull now

--- Transformer_Power/Page2.xaml.cs
+++ Transformer_Power/Page2.xaml.cs
@@ -17,6 +17,7 @@ using System.IO;
 using System.Data.SqlClient;
 using System.Text.RegularExpressions;
 using LiveCharts.Helpers;
+using LiveCharts.Geared;
 
 namespace Transformer_Power
 {
@@ -84,54 +85,57 @@ namespace Transformer_Power
         {
             SeriesCollection = new SeriesCollection
             {
-                new LineSeries
+                new GLineSeries
                 {
                     Title = "Series 1",
                     //Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
-                    Values = H2_List.AsChartValues()
+                     Values = H2_List.AsGearedValues().WithQuality(Quality.Low),
+                    //Values = H2_List.AsChartValues(),
+                    Fill = Brushes.Transparent,
+                    StrokeThickness = .5,
+                    PointGeometry = null
                 },
-                new LineSeries
+                new GLineSeries
                 {
                     Title = "Series 2",
                     //Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
-                    Values = CO_List.AsChartValues(),
-                    //PointGeometry = null
+                    //Values = CO_List.AsChartValues(),
+                    Values = CO_List.AsGearedValues().WithQuality(Quality.Low),
+                    Fill = Brushes.Transparent,
+                    StrokeThickness = .5,
+                    PointGeometry = null
                 },
-                new LineSeries
+                new GLineSeries
                 {
                     Title = "Series 3",
                     //Values = new ChartValues<double> { 4,2,7,2,7 },
-                    Values = C2H2_List.AsChartValues(),
+                    //Values = C2H2_List.AsChartValues(),
+                    Values = C2H2_List.AsGearedValues().WithQuality(Quality.Low),
+                    Fill = Brushes.Transparent,
+                    StrokeThickness = .5,
+                    PointGeometry = null
                     //PointGeometry = DefaultGeometries.Square,
                     //PointGeometrySize = 15
                 },
-                new LineSeries
+                new GLineSeries
                 {
                     Title = "Series 4",
                     //Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
-                    Values = C2H4_List.AsChartValues()
+                    //Values = C2H4_List.AsChartValues(),
+                    Values = C2H4_List.AsGearedValues().WithQuality(Quality.Low),
+                    Fill = Brushes.Transparent,
+                    StrokeThickness = .5,
+                    PointGeometry = null
                 }
             };
 
             Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
             YFormatter = value => value.ToString("C");
 
-            ////modifying the series collection will animate and update the chart
-            //SeriesCollection.Add(new LineSeries
-            //{
-            //    Title = "Series 4",
-            //    Values = new ChartValues<double> { 5, 3, 2, 4 },
-            //    LineSmoothness = 0, //0: straight lines, 1: really smooth lines
-            //    PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-            //    PointGeometrySize = 50,
-            //    PointForeground = Brushes.Gray
-            //});
-
-            ////modifying any series values will also animate and update the chart
-            //SeriesCollection[3].Values.Add(5d);
 
             DataContext = this;
         }
+
 
         List<double> H2_List = new List<double>();
         List<double> CO_List = new List<double>();
